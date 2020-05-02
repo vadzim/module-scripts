7fd70a2cd551c56b1cc2ce95509ad67e2f2bcc6e
Vadzim Zieńka <v@vadzim.info>
2020-05-02 18:35:11 +0300

    rename bin -> scripts

--- scripts/module-scripts.js
+++ scripts/module-scripts.js
@@ -3,11 +3,9 @@
 const { cli, ExitCode, ExitMessage } = require("../lib/cli")
 
 cli(process.argv).catch(e => {
-	if (e instanceof ExitCode)
-		process.exit(+e.message)
+	if (e instanceof ExitCode) process.exit(+e.message)
 	else if (e instanceof ExitMessage) {
 		console.error(e.message)
 		process.exit(1)
-	} else
-		throw e
+	} else throw e
 })
