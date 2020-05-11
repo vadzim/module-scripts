c352383f6c77beb47e4cc34955b7d86e5e255b25
Vadzim Zieńka <v@vadzim.info>
2020-05-11 09:21:11 +0300

    install latest version on init

--- packages/init-module-scripts/index.js
+++ packages/init-module-scripts/index.js
@@ -1,13 +1,18 @@
+const fs = require("fs").promises
 const { installDependency } = require("../../lib/packageJson/installDependency")
 const { spawn } = require("../../lib/utils/spawn")
 const { cliApp } = require("../../lib/utils/cliApp")
 const { name } = require("../../package.json")
 
 cliApp(async argv => {
-	if (argv[0] === "init" && argv.length === 1) {
+	if (argv[2] === "init" && argv.length === 3) {
 		await installDependency(name)
-		await spawn(name, ["init"])
+		await spawn(`node_modules/.bin/${name}`, ["init", "--no-install"])
 		return
 	}
-	console.log("Run `nmx init` to install module-scripts in your project.")
+	if (!fs.stat(`node_modules/.bin/${name}`).catch(() => false)) {
+		console.log(`Run \`nmx init\` to install ${name} in your project.`)
+		return
+	}
+	await spawn(`node_modules/.bin/${name}`, argv.slice(2))
 })
